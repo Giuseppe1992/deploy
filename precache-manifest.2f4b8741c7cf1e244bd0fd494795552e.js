44194a63a674f89e9b3c4e34f6d9dc49f0bae8c5
Giuseppe1992 <giuseppedilena92@gmail.com>
2020-09-06 23:11:23 +0200

    Updates

---
+++ precache-manifest.2f4b8741c7cf1e244bd0fd494795552e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fac5adbbd7c6402e2b74fec88c37a73",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "a5618293a84f0c7803a3",
+    "url": "/deploy/static/css/3.879e9400.chunk.css"
+  },
+  {
+    "revision": "d2dca3c2a3f90556143e",
+    "url": "/deploy/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "6bdcbd228c05ba564f3b",
+    "url": "/deploy/static/css/main.e37e71bf.chunk.css"
+  },
+  {
+    "revision": "52126fa62203a80dfaf4",
+    "url": "/deploy/static/js/2.55b343c7.chunk.js"
+  },
+  {
+    "revision": "7e0fd0c09333f2e525fa479f52af448a",
+    "url": "/deploy/static/js/2.55b343c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5618293a84f0c7803a3",
+    "url": "/deploy/static/js/3.b223b694.chunk.js"
+  },
+  {
+    "revision": "d2dca3c2a3f90556143e",
+    "url": "/deploy/static/js/4.6be21fe2.chunk.js"
+  },
+  {
+    "revision": "6bdcbd228c05ba564f3b",
+    "url": "/deploy/static/js/main.b67b0af5.chunk.js"
+  },
+  {
+    "revision": "dc0ea539107914c5e0f1",
+    "url": "/deploy/static/js/runtime-main.e35ce3ee.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/deploy/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "3a27169d740b3ddd55d222c8c7ef7717",
+    "url": "/deploy/static/media/Distrinet.3a27169d.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/deploy/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c5a04932ec7e6697919f21f744341760",
+    "url": "/deploy/static/media/azure_fundamentals.c5a04932.png"
+  },
+  {
+    "revision": "6a413df72df2894bcb5d7f797f26818e",
+    "url": "/deploy/static/media/cm.6a413df7.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/deploy/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/deploy/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "cbcbe74c23d615170ed783dffecc792f",
+    "url": "/deploy/static/media/intecsLogo.cbcbe74c.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deploy/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/deploy/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ccc59d235201628bcdab22eb897b0d01",
+    "url": "/deploy/static/media/orangeLogo.ccc59d23.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/deploy/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/deploy/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
